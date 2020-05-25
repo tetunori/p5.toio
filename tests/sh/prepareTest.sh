c87469d600024f381ee4fdf2647f1bccebfc3d3e
tetunori <tetunori@gmail.com>
2020-05-25 21:40:29 +0900

    able to test common

--- tests/sh/prepareTest.sh
+++ tests/sh/prepareTest.sh
@@ -5,8 +5,7 @@ TARGET_FILE="./tests/target.ts"
 rm $TARGET_FILE
 
 # Concatenate test target files
-cat ./global.d.ts\
-    ./src/common/util.ts\
+cat ./src/common/util.ts\
     ./src/id/position/mat.ts\
     ./src/id/position/TileMat.ts\
     ./src/id/position/colorTileMat.ts\
